e240d1c4a66f8afe60ead6b5f09528ab0f6690f4
Apoorv Thapliyal <apoorv1009@gmail.com>
2024-12-03 21:59:01 -0500

    add Task 2.05: Provide setpoints for each drone

--- src/px4_swarm_contoller/libs/Control/Control.cpp
+++ src/px4_swarm_contoller/libs/Control/Control.cpp
@@ -13,15 +13,60 @@
 
 ctrl::Control::Control() : Node("Control"){
 
-    // Initialize the setpoint counter
-    offboard_setpoint_counter_ = 0;
-
-    // Initialize the number of drones in the swarm
-    num_drones_ = 5;
-
     std::cout << "Control node initialized" << std::endl;
 
-    // Initalize the publishers for every drone in the swarm
+    // Load the YAML file
+    YAML::Node config = YAML::LoadFile("src/px4_swarm_contoller/config/config.yaml");
+
+    // Get the number of drones in the swarm
+    num_drones_ = config["num_drones"].as<int>();
+    std::cout << "Number of drones in the swarm: " << num_drones_ << std::endl;
+
+    // Resize the setpoints vector
+    setpoints_.resize(num_drones_);
+
+    // Resize the local positions vector
+    local_positions_.resize(num_drones_);
+
+    // Resize the vehicle local position subscribers vector
+    vehicle_local_position_subscribers_.resize(num_drones_);
+
+    // Resize the reached setpoint vector
+    reached_setpoint_.resize(num_drones_);
+
+    // Parse setpoints
+    YAML::Node sp_node = config["setpoints"];
+
+    // Iterate over each drone
+    for(auto it=sp_node.begin(); it!=sp_node.end(); it++){
+
+        // Get the drone ID, subtract 1 for 0 based indexing
+        int drone_id = std::stoi(it->first.as<std::string>()) - 1;
+
+        // Get the setpoints for the drone  
+        YAML::Node sp_list = it->second;
+
+        for (size_t i = 0; i < sp_list.size(); ++i) {
+            std::array<float, 4> sp;
+            for (size_t j = 0; j < 4; ++j) {
+                sp[j] = sp_list[i][j].as<float>();
+            }
+            setpoints_[drone_id].push_back(sp);
+        }
+    }
+
+    // Get the number of setpoints for each drone
+    num_setpoints_ = setpoints_[0].size();
+
+    // std::cout << "Number of setpoints for each drone: " << num_setpoints_ << std::endl;
+
+    // Initialize the offboard setpoint counter
+    offboard_setpoint_counter_ = 0;
+
+    // Initialise the setpoint counter
+    setpoint_counter_ = 0;
+
+    // Initalize the publishers and subscribers for every drone in the swarm
     for(int i=1; i<=num_drones_; i++){
 
         // String of the offboard control mode topic
@@ -33,6 +78,9 @@ ctrl::Control::Control() : Node("Control"){
         // String of the vehicle command topic
         std::string vehicle_command_topic = "/px4_" + std::to_string(i) + "/fmu/in/vehicle_command";
 
+        // String of the local position topic
+        std::string local_position_topic = "/px4_" + std::to_string(i) + "/fmu/out/vehicle_local_position";
+
         // Publisher for the offboard control mode
         offboard_control_mode_publishers_.push_back(this->create_publisher<OffboardControlMode>(offboard_control_mode_topic, 10));
         
@@ -41,29 +89,58 @@ ctrl::Control::Control() : Node("Control"){
         
         // Publisher for the vehicle command
         vehicle_command_publishers_.push_back(this->create_publisher<VehicleCommand>(vehicle_command_topic, 10));
+
+        // Subscriber for the local position
+        auto sub = this->create_subscription<VehicleLocalPosition>(
+            local_position_topic, 
+            rclcpp::QoS(10).best_effort(), 
+            [this, i](const VehicleLocalPosition::SharedPtr msg) -> void {
+                this->vehicle_local_position_callback(i-1, msg);
+            });
+
+        // Store the subscriber in the subscribers vector
+        vehicle_local_position_subscribers_[i-1] = sub;
     }
 
+    // Set sent offboard_arm_sent to false
+    offboard_arm_sent_ = false;
 
-    // Define a timer callback
+    // Define a timer callback for controlling the drones
     auto timer_callback = [this]() -> void{
 
-        if(offboard_setpoint_counter_ == 10) {
+        // After enough setpoints and offboard control mode messages have been sent, arm the drones and switch to offboard mode
+        if(offboard_setpoint_counter_ == 50) {
             // Switch to offboard mode and arm the drones
             for(int i=0; i<num_drones_; i++){
-                arm(vehicle_command_publishers_[i]);
-                offboard_mode(vehicle_command_publishers_[i]);
+
+                // Send command multiple times to ensure that the command is received
+                int counter = 0;
+                while(counter < 5){
+                    arm(vehicle_command_publishers_[i]);
+                    offboard_mode(vehicle_command_publishers_[i]);
+                    counter++;
+                }
             }
+
+            // Set offboard_arm_sent to true
+            offboard_arm_sent_ = true;
         }
 
         // The offboard control mode needs to be paired with a trajectory setpoint
-        // Publish setpoint as a 5m altitude hold 
         for(int i=0; i<num_drones_; i++){
             publish_offboard_control_mode(offboard_control_mode_publishers_[i]);
-            publish_trajectory_setpoint(trajectory_setpoint_publishers_[i], 0.0, 0.0, -5.0, 0.0);
+
+            // Till arm_offboard_sent is not set, publish default setpoints
+            if(!offboard_arm_sent_)
+                // Publish setpoint as a 5m altitude hold 
+                publish_trajectory_setpoint(trajectory_setpoint_publishers_[i], 0.0, 0.0, -5.0, 0.0);
+
+            // After arming and switching to offboard mode, publish the setpoints read from the yaml file
+            publish_default_setpoints();
         }
 
-        // Stop the counter after reaching 11
-        if(offboard_setpoint_counter_ < 11) {
+        // Stop the counter after reaching certain threshold
+        if(offboard_setpoint_counter_ < 51) {
             offboard_setpoint_counter_++;
         }
     };
@@ -117,3 +194,52 @@ void ctrl::Control::publish_vehicle_command(rclcpp::Publisher<VehicleCommand>::S
     pub_->publish(msg);
 }
 
+void ctrl::Control::publish_default_setpoints(){
+    // Iterate over the drones
+    for(int i=0; i<num_drones_; i++){
+
+        // Get the current setpoint to be published
+        std::array<float, 4> setpoint = setpoints_[i][setpoint_counter_];
+
+        // Publish the setpoint
+        publish_trajectory_setpoint(trajectory_setpoint_publishers_[i], setpoint[0], setpoint[1], setpoint[2], setpoint[3]);
+
+        // Calculate the distance of the drone from its setpoint
+        float distance = std::sqrt(std::pow(local_positions_[i][0] - setpoint[0], 2) + 
+                         std::pow(local_positions_[i][1] - setpoint[1], 2) +
+                         std::pow(local_positions_[i][2] - setpoint[2], 2));
+
+        // If the drone is close to its setpoint and has not reached its setpoint before, set the reached_setpoint_ flag to true
+        if(distance < 0.3 and !reached_setpoint_[i]) {
+            reached_setpoint_[i] = true;
+        }
+
+        // If all the drones have reached their setpoints, increment the setpoint_counter_, but stop it after reaching the last setpoint
+        if(std::all_of(reached_setpoint_.begin(), reached_setpoint_.end(), [](bool v) { return v; })) {
+            setpoint_counter_++;
+            if(setpoint_counter_ == num_setpoints_) {
+                setpoint_counter_ = num_setpoints_ - 1;
+            }
+
+            std::cout << "Reached setpoint " << setpoint_counter_ << std::endl;
+
+            // Reset the reached_setpoint_ flag
+            std::fill(reached_setpoint_.begin(), reached_setpoint_.end(), false);
+        }
+
+        // // If the drone has not reached its setpoint
+        // if(!reached_setpoint_[i]){
+        //     // Print current position and setpoint
+        //     // std::cout << "Drone " << i+1 << " current position: x=" << local_positions_[i][0] << ", y=" << local_positions_[i][1] << ", z=" << local_positions_[i][2] << std::endl;
+        //     // std::cout << "Drone " << i+1 << " setpoint: x=" << setpoint[0] << ", y=" << setpoint[1] << ", z=" << setpoint[2] << std::endl;
+
+        //     // Print the distance from the setpoint
+        //     std::cout << "Drone " << i+1 << " distance from setpoint: " << distance << std::endl;
+        // }
+    }
+}
+
+void ctrl::Control::vehicle_local_position_callback(int drone_id, const VehicleLocalPosition::SharedPtr msg){
+    local_positions_[drone_id] = {msg->x, msg->y, msg->z};
+    // std::cout << "Drone " << drone_id << " local position: x=" << msg->x << ", y=" << msg->y << ", z=" << msg->z << std::endl;
+}
